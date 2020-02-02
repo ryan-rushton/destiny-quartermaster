4347d9798238d51994abe1467802008f0a57bf8e
Ryan Rushton <ryan.rushton79@gmail.com>
2020-02-02 11:48:43 +1100

    More config and got rid of the App test for now

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-// test("It renders", () => {
-//     const renderedApp = render(<App />).baseElement;
-//     expect(renderedApp).toBeInTheDocument();
-// });
